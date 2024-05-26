b807ee87c3de34546cc66d8bf9c41af56d0ee828
terryxper <93462767+terryxper@users.noreply.github.com>
2024-05-26 23:27:59 +0800

    Update index.ts

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -1,22 +1,5 @@
 import cn from "./cn";
 import en from "./en";
-import pt from "./pt";
-import tw from "./tw";
-import id from "./id";
-import fr from "./fr";
-import es from "./es";
-import it from "./it";
-import tr from "./tr";
-import jp from "./jp";
-import de from "./de";
-import vi from "./vi";
-import ru from "./ru";
-import no from "./no";
-import cs from "./cs";
-import ko from "./ko";
-import ar from "./ar";
-import bn from "./bn";
-import sk from "./sk";
 import { merge } from "../utils/merge";
 
 import type { LocaleType } from "./cn";
@@ -25,23 +8,6 @@ export type { LocaleType, PartialLocaleType } from "./cn";
 const ALL_LANGS = {
   cn,
   en,
-  tw,
-  pt,
-  jp,
-  ko,
-  id,
-  fr,
-  es,
-  it,
-  tr,
-  de,
-  vi,
-  ru,
-  cs,
-  no,
-  ar,
-  bn,
-  sk,
 };
 
 export type Lang = keyof typeof ALL_LANGS;
@@ -49,25 +15,8 @@ export type Lang = keyof typeof ALL_LANGS;
 export const AllLangs = Object.keys(ALL_LANGS) as Lang[];
 
 export const ALL_LANG_OPTIONS: Record<Lang, string> = {
-  cn: "简体中文",
+  cn: "中文",
   en: "English",
-  pt: "Português",
-  tw: "繁體中文",
-  jp: "日本語",
-  ko: "한국어",
-  id: "Indonesia",
-  fr: "Français",
-  es: "Español",
-  it: "Italiano",
-  tr: "Türkçe",
-  de: "Deutsch",
-  vi: "Tiếng Việt",
-  ru: "Русский",
-  cs: "Čeština",
-  no: "Nynorsk",
-  ar: "العربية",
-  bn: "বাংলা",
-  sk: "Slovensky",
 };
 
 const LANG_KEY = "lang";
@@ -129,7 +78,7 @@ export function changeLang(lang: Lang) {
 export function getISOLang() {
   const isoLangString: Record<string, string> = {
     cn: "zh-Hans",
-    tw: "zh-Hant",
+    cn: "zh-Hant",
   };
 
   const lang = getLang();
