0574a7146128bf718c0bb3d881cd288e4b1f274e
terryxper <93462767+terryxper@users.noreply.github.com>
2024-05-26 09:16:46 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           SAGE ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Your AI assistant.
+          Your AI assistant. (GPT-4o)
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
